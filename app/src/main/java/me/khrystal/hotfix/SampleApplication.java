69d752f55da10f1116e83c071aca5155f41ba449
kHRYSTAL <723526676@qq.com>
2017-01-03 19:06:39 +0800

    update master

--- app/src/main/java/me/khrystal/hotfix/SampleApplication.java
+++ app/src/main/java/me/khrystal/hotfix/SampleApplication.java
@@ -21,7 +21,7 @@ public class SampleApplication extends TinkerApplication {
      */
     public SampleApplication() {
         super(ShareConstants.TINKER_ENABLE_ALL,
-                "me.khrystal.hotfix.SampleApplicationLike",
+                "me.khrystal.hotfix.SampleApplicationDelegate",
                 "com.tencent.tinker.loader.TinkerLoader", false);
     }
 }
